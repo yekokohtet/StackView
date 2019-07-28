a98e51ca7c4ca68d585dd11a992f5fc0d5bdfd9a
Ye Ko Ko Htet <yekokohtet@mm-digital-solutions.com>
2019-07-28 10:35:35 +0630

    - stackview

---
+++ StackView-Day2/StackView-Day2/StackView-Day2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView-Day2
+//
+//  Created by Ye Ko Ko Htet on 28/07/2019.
+//  Copyright © 2019 Ye Ko Ko Htet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
